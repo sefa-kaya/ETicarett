4c2741048a6a12423c8a83c27ef8436f4f3a94f1
sefa-kaya <sefakaya505@gmail.com>
2023-07-08 19:34:53 +0300

    ETicarettt

---
+++ ShoppingAPI.Entity/Poco/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ShoppingAPI.Entity.Base;
+
+namespace ShoppingAPI.Entity.Poco
+{
+    public class User : AuditableEntity
+    {
+        public User()
+        {
+            this.Orders= new HashSet<Order>();  
+        }
+
+        public string? FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adress { get; set; }
+        public virtual IEnumerable<Order> Orders { get; set; }
+
+    }
+}
